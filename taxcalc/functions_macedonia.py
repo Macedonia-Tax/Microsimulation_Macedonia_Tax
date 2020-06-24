d541438f190501827e9ed9e1c36440f40e6d3ee7
Sana Elahi <selahi@worldbank.org>
2020-06-24 12:27:43 -0400

    chnages so far

---
+++ taxcalc/functions_macedonia.py
@@ -0,0 +1,87 @@
+"""
+pitaxcalc-demo functions that calculate personal income tax liability.
+"""
+# CODING-STYLE CHECKS:
+# pycodestyle functions.py
+# pylint --disable=locally-disabled functions.py
+
+import math
+import copy
+import numpy as np
+from taxcalc.decorators import iterate_jit
+
+
+@iterate_jit(nopython=True)
+def net_salary_income(SALARIES, Income_Salary):
+    """
+    Compute net salary as gross salary minus deductions u/s 16.
+    """
+    # TODO: when gross salary and deductions are avaiable, do the calculation
+    # TODO: when using net_salary as function argument, no calculations neeed
+    """
+    The deductions (transport and medical) that are being done away with while
+    intrducing Standard Deduction is not captured in the schedule also. Thus,
+    the two deductions combined (crude estimate gives a figure of 30000) is
+    added to "SALARIES" and then "std_deduction" (introduced as a policy
+    variable) is deducted to get "Income_Salary". Standard Deduction is being
+    intruduced only from AY 2019 onwards, "std_deduction" is set as 30000 for
+    AY 2017 and of 2018 thus resulting in no change for those years.
+    """
+    Income_Salary = SALARIES 
+    return Income_Salary
+
+@iterate_jit(nopython=True)
+def calc_income_house_property(HP_deduction, INCOME_HP, Income_House_Property):
+    """
+    Compute income from house property.
+    """
+    Income_House_Property = INCOME_HP - HP_deduction
+    if (Income_House_Property<0):
+        Income_House_Property = 0
+    return Income_House_Property
+
+@iterate_jit(nopython=True)
+def calc_tax_LTCG(LT_CG_RATE1, LT_CG_RATE2, LT_CG_AMT_1, LT_CG_AMT_2, Tax_LT_CG_RATE1, Tax_LT_CG_RATE2):
+    """
+    Computes The Long Term Capital Gains.
+    """
+    Tax_LT_CG_RATE1 = LT_CG_RATE1 * LT_CG_AMT_1
+    Tax_LT_CG_RATE2 = LT_CG_RATE2 * LT_CG_AMT_2    
+    return (Tax_LT_CG_RATE1, Tax_LT_CG_RATE2)
+
+@iterate_jit(nopython=True)
+def gross_total_income(Income_Salary, GTI):
+    """
+    Compute GTI including capital gains amounts taxed at special rates.
+    """
+    GTI = Income_Salary
+    return GTI
+
+
+@iterate_jit(nopython=True)
+def taxable_total_income(GTI, deductions, TTI):
+    """
+    Compute TTI.
+    """
+    TTI = GTI 
+    return TTI
+
+@iterate_jit(nopython=True)
+def pit_liability(rate1, rate2, rate3, rate4, tbrk1, tbrk2, tbrk3, tbrk4,
+                  TTI, Tax_LT_CG_RATE1, Tax_LT_CG_RATE2, pitax):
+    """
+    Compute tax liability given the progressive tax rate schedule specified
+    by the (marginal tax) rate* and (upper tax bracket) brk* parameters and
+    given taxable income (taxinc)
+
+    Subtract 'TI_special_rates' from 'TTI' to get the portion of total income
+    that is taxed at normal rates. Now add agricultural income (income used for
+    rate purpose only) to get Aggregate_Income.
+    """
+    # subtract TI_special_rates from TTI to get Aggregate_Income, which is
+    # the portion of TTI that is taxed at normal rates
+    taxinc = TTI 
+    tax = (rate1 * min(taxinc, tbrk1) +
+                       rate2 * max(0., taxinc - tbrk2))
+    pitax = tax 
+    return (pitax)
